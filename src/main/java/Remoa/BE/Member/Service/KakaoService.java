885966c88e432302d0a98c20ac9b43dd76e4a6c9
jayrhie <vhy97z@naver.com>
2023-03-04 13:02:12 +0900

    Kakao Login 한글 깨짐 오류 수정

--- src/main/java/Remoa/BE/Member/Service/KakaoService.java
+++ src/main/java/Remoa/BE/Member/Service/KakaoService.java
@@ -14,6 +14,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -121,7 +122,8 @@ public class KakaoService {
             int responseCode = urlConnection.getResponseCode();
             log.debug("responseCode = {}", responseCode);
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),
+                    StandardCharsets.UTF_8)); //한글 깨짐 문제 해결하기 위한 인코딩 파라메터 추가
             String line = "";
             String res = "";
             //다양한 형식(한 줄 이상의 JSON 데이터)를 받기 위한 작업
