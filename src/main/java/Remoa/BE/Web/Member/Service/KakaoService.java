d9f047bed2fbd6ecfb4dd1d50e66e61cfc7b4140
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2024-07-19 16:29:03 +0900

    FIX : 배포 프론트 서버로 수정

--- src/main/java/Remoa/BE/Web/Member/Service/KakaoService.java
+++ src/main/java/Remoa/BE/Web/Member/Service/KakaoService.java
@@ -117,7 +117,8 @@ public class KakaoService {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
         params.add("client_id", "415ea3ee0124fa3538ce37d80adc94c2");
-        params.add("redirect_uri", "http://localhost:3000/login/kakao");
+        //params.add("redirect_uri", "http://localhost:3000/login/kakao");
+        params.add("redirect_uri", "https://d197wa6gufmlpc.cloudfront.net");
         params.add("code", code);
         //params.add("client_secret", client_secret);
 
