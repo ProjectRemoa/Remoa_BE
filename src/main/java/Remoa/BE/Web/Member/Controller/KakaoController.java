daa708fb355a246de857ef35a287db16e1fcb67e
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2024-07-23 22:02:59 +0900

    FIX : 카카오 로그인 수정

--- src/main/java/Remoa/BE/Web/Member/Controller/KakaoController.java
+++ src/main/java/Remoa/BE/Web/Member/Controller/KakaoController.java
@@ -56,11 +56,11 @@ public class KakaoController {
     @GetMapping("/login/kakao")
     @Operation(summary = "카카오 로그인", description = "카카오 로그인을 통해 사용자를 식별하고 로그인 또는 회원가입 처리합니다.")
     public ResponseEntity<BaseResponse<KakaoLoginResponseDto>> getCI(@RequestParam String code,
-                                                                     @RequestParam("request_uri") String requestUri){
+                                                                     @RequestParam("redirect_uri") String redirectUri){
         log.info("EndPoint Get /login/kakao");
 
         log.info("code = " + code);
-        KakaoLoginResponseDto kakaoLoginResponseDto = kakaoService.kakaoLogin(code, requestUri);
+        KakaoLoginResponseDto kakaoLoginResponseDto = kakaoService.kakaoLogin(code, redirectUri);
 
         if(kakaoLoginResponseDto.isSignup()){
             BaseResponse<KakaoLoginResponseDto> response = new BaseResponse<>(CustomMessage.OK, kakaoLoginResponseDto);
